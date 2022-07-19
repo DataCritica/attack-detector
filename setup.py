7650f4e4e279c9aa720c52099bb61151e187b73f
Fer Aguirre <kay_op@protonmail.com>
2022-07-19 17:14:28 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Attack Detector',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Repository for the Attack Detector team from the 2022 JournalismAI Fellowship',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
